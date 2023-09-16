00ff0b4dc64b8608abfda5fe19e705386ea5df84
Abhishek Pandey <abhishekpandey3188@gmail.com>
2023-09-16 19:46:31 +0530

    Initialize

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useUserContext } from "./context/UserContext";
 
 function App() {
+  const { text } = useUserContext();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="underline">{text}</div>
+    </>
   );
 }
 
